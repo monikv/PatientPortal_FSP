3c99bd41ec9d5f9122cfdf58e40602e8c7ce41a0
Aravind Jitta <rvindjitta11@gmail.com>
2024-04-03 14:40:21 -0500

    Added auth details in local storage

--- React-frontend/src/components/AuthContext.tsx
+++ React-frontend/src/components/AuthContext.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -13,10 +19,21 @@ interface AuthProviderProps {
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+    // Get the authentication status from local storage and convert it to boolean
+    const isAuth = localStorage.getItem("isAuthenticated");
+    return isAuth === "true";
+  });
 
-  const login = () => setIsAuthenticated(true);
-  const logout = () => setIsAuthenticated(false);
+  const login = () => {
+    localStorage.setItem("isAuthenticated", "true"); // Store authentication status in local storage
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.setItem("isAuthenticated", "false"); // Update local storage when logging out
+    setIsAuthenticated(false);
+  };
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
@@ -28,7 +45,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
